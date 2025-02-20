ba20b9b0d9f7429817b5efb4d1d6dda8a04da196
Lucas Matias <matiasguimaraes.10@gmail.com>
2025-02-20 01:20:42 -0300

    verificacao

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const title = "Olá, meu nome é Lucas Matias";
+    const paragraph = `Sou um Desenvolvedor front-end com experiência em criar interfaces responsivas e otimizadas.\n
+    Tenho conhecimento em diversas tecnologias front-end e back-end, incluindo React, PHP, NodeJS e .NET.\n
+    Gosto de ter foco em melhoria de UI/UX design e responsividade.\n
+    Mesmo sabendo diversas tecnologias, meu foco é o front-end.`;
+
+    function typeEffect(element, text, speed, callback) {
+        let i = 0;
+        function typing() {
+            if (i < text.length) {
+                element.innerHTML = text.substring(0, i).replace(/\n/g, "<br>");
+                i++;
+                setTimeout(typing, speed);
+            } else if (callback) {
+                callback();
+            }
+        }
+        typing();
+    }
+
+    const titleElement = document.getElementById("typewriter");
+    const textElement = document.getElementById("typewriter-text");
+
+    typeEffect(titleElement, title, 25, function () {
+        typeEffect(textElement, paragraph, 15);
+    });
+});
